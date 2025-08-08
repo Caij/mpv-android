b09dc0955b99286addd6f34b26b33abd74b37c94
Caij <271945881@qq.com>
2025-08-08 15:45:33 +0800

    修改依赖问题

--- buildscripts/download.sh
+++ buildscripts/download.sh
@@ -2,3 +2,7 @@
 
 ./include/download-sdk.sh
 ./include/download-deps.sh
+
+sed -i '' "s/dependency('libswscale', version: '>= 7.5.100')/dependency('libswscale', version: '>= 7.5.100', required: false)/" deps/mpv/meson.build
+
+cat deps/mpv/meson.build
