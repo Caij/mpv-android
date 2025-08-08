10dc11fed3ed02b02a1fb5900554ee55ebe69360
Caij <271945881@qq.com>
2025-08-08 18:03:00 +0800

    删除

--- buildscripts/download.sh
+++ buildscripts/download.sh
@@ -3,6 +3,5 @@
 ./include/download-sdk.sh
 ./include/download-deps.sh
 
-sed -i "s/dependency('libswscale', version: '>= 7.5.100')/dependency('libswscale', version: '>= 7.5.100', required: false)/" deps/mpv/meson.build
-
-cat deps/mpv/meson.build
+#sed -i "s/dependency('libswscale', version: '>= 7.5.100')/dependency('libswscale', version: '>= 7.5.100', required: false)/" deps/mpv/meson.build
+#cat deps/mpv/meson.build
