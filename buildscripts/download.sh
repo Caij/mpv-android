ff5fb73c915ee924fc7976823b29a5e22828d1c4
Caij <271945881@qq.com>
2025-08-08 19:59:35 +0800

    缩减体积

--- buildscripts/download.sh
+++ buildscripts/download.sh
@@ -2,6 +2,3 @@
 
 ./include/download-sdk.sh
 ./include/download-deps.sh
-
-#sed -i "s/dependency('libswscale', version: '>= 7.5.100')/dependency('libswscale', version: '>= 7.5.100', required: false)/" deps/mpv/meson.build
-#cat deps/mpv/meson.build
